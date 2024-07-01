3af0ee161ae63ee0cd393aaa632ba9df2aba74bd
Jeff Claybrook <jeffclaybrook@Jeffs-MacBook-Pro.local>
2024-07-01 16:26:47 -0500

    Updated padding

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -28,7 +28,7 @@ const Page = () => {
      <CopyButton />
      <p className="mb-4 leading-8">Every worthwhile undertaking needs a start-up plan.</p>
      <p className="mb-4 leading-8"><strong>Stage One:</strong></p>
-     <ol className="mb-8 leading-8 list-decimal">
+     <ol className="mb-8 leading-8 list-decimal px-4">
       <li>Rely on God, which is self-explanatory.</li>
       <li>Formulate  a vision and put it into writing. Hopefully, this website suffices.</li>
       <li>Create a plan for putting the vision into action and keeping it simple.</li>
@@ -40,12 +40,12 @@ const Page = () => {
       <li>See how this works out and go from there. Hopefully, multiplication kicks in and evenutally a &quot;movement&quot; develops.</li>
      </ol>
      <p className="mb-8 leading-4"><strong>Stage Two:</strong></p>
-     <ol className="mb-8 leading-8 list-decimal">
+     <ol className="mb-8 leading-8 list-decimal px-4">
       <li>Address the issue of &quot;highly interested&quot; people staying connected.</li>
       <li>Direct people to the book, <em>Never Before in History-America&apos;s Inspired Birth,</em> published by the Discovery Institute, Seattle, WA.</li>
      </ol>
      <p className="mb-4 leading-8"><strong>Stage Three:</strong></p>
-     <ol className="mb-8 leading-8 list-decimal">
+     <ol className="mb-8 leading-8 list-decimal px-4">
       <li>Set up a system for those who wish to memorize some or all of the Declaration.</li>
       <li>Everywhere he goes, he hears, &quot;I coud never do what you do.&quot; This is because people are lost when it comes to memorization. He directs people who adhere to the &quot;Muble, Grumble, and Fumble System&quot; to the &quot;Five Flawless Times System.&quot;</li>
      </ol>
