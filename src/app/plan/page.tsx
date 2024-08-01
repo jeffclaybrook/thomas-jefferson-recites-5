687ea8bb2aad894ec5dfc75355967bf60cf358cc
Jeff Claybrook <jeffclaybrook@Jeffs-MBP.home>
2024-08-01 13:44:50 -0500

    Updated the Plan

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -20,36 +20,19 @@ const Page = () => {
       className="mx-auto rounded-md my-16"
      />
      <h2 className="text-2xl font-semibold mb-2">The Plan</h2>
-     <h3 className="mb-2">Launching the Declaration onto the National Stage</h3>
-     <h3 className="mb-8">By Craig L. Claybrook</h3>
+     <h3 className="mb-2">Launching the Declaration onto the National Stage. Every worthwhile undertaking needs a start-up plan.</h3>
      <video width="720" height="480" controls className="mb-8">
       <source src="/videos/short_video.mp4" type="video/mp4" />
      </video>
      <CopyButton />
-     <p className="mb-4 leading-8">Every worthwhile undertaking needs a start-up plan.</p>
-     <p className="mb-4 leading-8"><strong>Stage One:</strong></p>
-     <ol className="mb-8 leading-8 list-decimal px-4">
-      <li>Rely on God, which is self-explanatory.</li>
-      <li>Formulate  a vision and put it into writing. Hopefully, this website suffices.</li>
-      <li>Create a plan for putting the vision into action and keeping it simple.</li>
-      <ul className="list-disc px-4">
-       <li>Place a 60-second video of the Declaration&apos;s most potent verbiage on their particular social media.</li>
-       <li>Include the message &quot;to see the full recitation, go to www.thomasjeffersonrecites.com.&quot;</li>
-       <li>Ask for people to cut and paste and then forward this shorter version video to three or more of their friends.</li>
-      </ul>
-      <li>See how this works out and go from there. Hopefully, multiplication kicks in and evenutally a &quot;movement&quot; develops.</li>
-     </ol>
-     <p className="mb-8 leading-4"><strong>Stage Two:</strong></p>
-     <ol className="mb-8 leading-8 list-decimal px-4">
-      <li>Address the issue of &quot;highly interested&quot; people staying connected.</li>
-      <li>Direct people to the book, <em>Never Before in History-America&apos;s Inspired Birth,</em> published by the Discovery Institute, Seattle, WA.</li>
-     </ol>
-     <p className="mb-4 leading-8"><strong>Stage Three:</strong></p>
-     <ol className="mb-8 leading-8 list-decimal px-4">
-      <li>Set up a system for those who wish to memorize some or all of the Declaration.</li>
-      <li>Everywhere he goes, he hears, &quot;I coud never do what you do.&quot; This is because people are lost when it comes to memorization. He directs people who adhere to the &quot;Muble, Grumble, and Fumble System&quot; to the &quot;Five Flawless Times System.&quot;</li>
-     </ol>
-     <p className="mb-4 leading-8">This video markes the climatic conclusion of the Declration. Mr. Claybrook hopes that people will find this website worthy of passing along, that they will cut and paste this snippit to send to three or more friends.</p>
+     <h4 className="leading-8 font-bold">Stage One:</h4>
+     <p className="mb-4 leading-8">His motivation is to glorify God by launching the Declaration onto the national stage. (The Declaration contains four references to God.)</p>
+     <h4 className="leading-8 font-bold">Stage Two:</h4>
+     <p className="mb-4 leading-8">His plan is to help people do just that, by offering them a short video that would direct them to this website so, that they can share this with three or more friends.</p>
+     <h4 className="leading-8 font-bold">Final Comments:</h4>
+     <p className="mb-4 leading-8">He recommends the book <a href="https://www.amazon.com/Never-Before-History-Americas-Inspired/dp/0964210428" target="_blank" rel="noreferrer"><em>Never Before in History--America&apos;s Inspired Birth,</em></a> published by the Discovery Institute, Seattle, WA.</p>
+     <p className="mb-4 leading-8">For those who wish to memorize all or part of the Declaration, he will be adding a brief summary of &quot;how to memorize&quot; to this website.</p>
+     <p className="mb-4 leading-8">This brief video above presents the climactic conclusion of the Declaration. Mr Claybrook hopes that people will find this website worthy of passing along. At age 77, he is more interested in people watching him via video than &quot;in person&quot; He may be reached at craigclaybrook@gmail.com.</p>
     </Section>
    </Main>
   </>
